2d9ceaec7f24b11abd2df457f3bb9c9c036dee6c
Oluwaferanmi <alabaaderonketoyin@gmail.com>
2025-06-21 10:55:24 +0100

    completed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,11 +10,11 @@ const {
 const authMiddleware = require('../middleware/authMiddleware');
 const adminMiddleware = require('../middleware/adminMiddleware');
 
-// User routes
+
 router.get('/profile', authMiddleware, getProfile);
 router.post('/earn', authMiddleware, earnPoints);
 
-// Admin routes
+
 router.get('/', authMiddleware, adminMiddleware, getAllUsers);
 router.put('/promote/:userId', authMiddleware, adminMiddleware, promoteToAdmin);
 
